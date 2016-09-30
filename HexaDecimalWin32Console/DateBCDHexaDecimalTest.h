218c3fda1f00bc1d6ae5f77c00cb03bac75c8556
b50061tem <b50061tem@OEC603>
2016-09-30 15:16:07 +0900

    作業状態の保存

---
+++ HexaDecimalWin32Console/DateBCDHexaDecimalTest.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "DateBCDHexaDecimal.h"
+
+class DateBCDHexaDecimalTest
+{
+private:
+
+    bool testSuccess;
+
+public:
+
+    bool GetTestSuccess();
+
+    void DateBCDHexaDecimalTest01();
+
+    void DateBCDHexaDecimalTest02();
+
+    DateBCDHexaDecimalTest();
+
+    ~DateBCDHexaDecimalTest();
+};
